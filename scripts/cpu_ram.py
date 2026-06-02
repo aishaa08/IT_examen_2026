e6b6ec0cfc630ba3f2e34ed45fcfd974ec77f8a2
aishaa08 <aisha.a.kurbanova@gmail.com>
2026-06-02 11:35:44 +0200

    fikt

--- scripts/cpu_ram.py
+++ scripts/cpu_ram.py
@@ -13,7 +13,7 @@ cursor = conn.cursor()
 cpu = psutil.cpu_percent()
 ram = psutil.virtual_memory().percent
 
-sql = "INSERT INTO system_status (cpu, ram) VALUES (%s, %s)"
+sql = "INSERT INTO system_cpu (cpu, ram) VALUES (%s, %s)"
 cursor.execute(sql, (cpu, ram))
 
 conn.commit()
